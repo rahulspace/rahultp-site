6e177586198b4f85620c9bcddf3e885415c536e2
rahulspace <rahultp518@gmail.com>
2018-11-16 10:36:43 +0530

    Change fonts

--- src/App.vue
+++ src/App.vue
@@ -17,25 +17,32 @@ export default {
 
 <style>
 
+
 @font-face {
-  font-family: NotaSans;
-  src: url(./assets/fonts/NotoSans-Bold.woff);
+  font-family: Lato;
+  src: url(./assets/fonts/Lato-Black.woff);
+  font-weight: 900;
+}
+
+@font-face {
+  font-family: Lato;
+  src: url(./assets/fonts/Lato-Bold.woff);
   font-weight: 700;
 }
 
 @font-face {
-  font-family: NotaSans;
-  src: url(./assets/fonts/NotoSans-Regular.woff);
+  font-family: Lato;
+  src: url(./assets/fonts/Lato-Regular.woff);
   font-weight: 400;
 }
 
 body {
   margin: 0;
-  font-family: NotaSans;
+  font-family: Lato;
 }
 
 h1, h2, h3, h4, h5, h6 {
-  font-family: NotaSans;
+  font-family: Lato;
 }
 
 #app {
