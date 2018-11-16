8f907bb3bf9d16ee804b8a13663c32bf6574aef0
rahulspace <rahultp518@gmail.com>
2018-11-16 10:24:14 +0530

    Import and set font

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,25 @@ export default {
 
 <style>
 
+@font-face {
+  font-family: NotaSans;
+  src: url(./assets/fonts/NotoSans-Bold.woff);
+  font-weight: 700;
+}
+
+@font-face {
+  font-family: NotaSans;
+  src: url(./assets/fonts/NotoSans-Regular.woff);
+  font-weight: 400;
+}
+
 body {
   margin: 0;
+  font-family: NotaSans;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: NotaSans;
 }
 
 #app {
